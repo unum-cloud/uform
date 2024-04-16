21f49bab444e0761ab7fc7ed20b3c81fb7924d17
Jake Zhang <ake020675@163.com>
2024-04-16 11:55:10 +0800

    Fix: Importing ViT in `gen_model.py` (#80)

--- python/uform/gen_model.py
+++ python/uform/gen_model.py
@@ -15,7 +15,7 @@ from transformers.models.auto.modeling_auto import (AutoModel,
 from transformers.processing_utils import ProcessorMixin
 from transformers.tokenization_utils_base import BatchEncoding
 
-from uform.models import VisualEncoder
+from uform.torch_models import VisualEncoder
 
 IMAGENET_MEAN = (0.48145466, 0.4578275, 0.40821073)
 IMAGENET_STD = (0.26862954, 0.26130258, 0.27577711)
