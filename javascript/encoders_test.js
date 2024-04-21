0c2aa2828693edd6002c34cdd87f43e196c7775a
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2024-04-21 05:19:23 +0000

    Improve: Test many models in JS

--- javascript/encoders_test.js
+++ javascript/encoders_test.js
@@ -1,4 +1,6 @@
-import { existsSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 import { getCheckpoint, Modality } from "./hub.mjs";
 import { TextProcessor, TextEncoder, ImageEncoder, ImageProcessor } from "./encoders.mjs";
@@ -9,83 +11,118 @@ function assert(condition, message) {
     }
 }
 
-async function testGetCheckpoint() {
-    console.log("Test getCheckpoint: Start");
-
-    try {
-        const modelId = 'unum-cloud/uform3-image-text-english-small';
-        const token = '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';
-        const modalities = [Modality.TextEncoder, Modality.ImageEncoder];
-
-        const { configPath, modalityPaths, tokenizerPath } = await getCheckpoint(
-            modelId,
-            modalities,
-            token,
-            '.onnx'
-        );
-
-        assert(configPath !== null, "Config path should not be null");
-        assert(modalityPaths !== null, "Modality paths should not be null");
-        assert(tokenizerPath !== null, "Tokenizer path should not be null");
-
-        // Check if the file actually exists
-        assert(existsSync(configPath), `Config file should exist at ${configPath}`);
-        assert(existsSync(tokenizerPath), `Tokenizer file should exist at ${tokenizerPath}`);
-        for (const modalityPath of Object.values(modalityPaths)) {
-            assert(existsSync(modalityPath), `Modality file should exist at ${modalityPath}`);
-        }
-
-        console.log("Test getCheckpoint: Success");
-    } catch (error) {
-        console.error("Test getCheckpoint: Failed", error);
+// Check if the HuggingFace Hub API token is set in the environment variable.
+let hf_token = process.env.HUGGINGFACE_HUB_TOKEN;
+if (!hf_token) {
+    const dirname = path.dirname(fileURLToPath(import.meta.url));
+    const tokenPath = path.join(dirname, '../', '.hf_token');
+    if (existsSync(tokenPath)) {
+        hf_token = readFileSync(tokenPath, 'utf8').trim();
     }
 }
 
-async function testEncoders() {
-    console.log("Test testEncoders: Start");
-    let textEncoder = null;
-    let imageEncoder = null;
+async function tryGettingCheckpoint(modelId, modalities) {
+    const { configPath, modalityPaths, tokenizerPath } = await getCheckpoint(
+        modelId,
+        modalities,
+        hf_token,
+        '.onnx'
+    );
+
+    assert(configPath !== null, "Config path should not be null");
+    assert(modalityPaths !== null, "Modality paths should not be null");
+    assert(tokenizerPath !== null, "Tokenizer path should not be null");
+
+    // Check if the file actually exists
+    assert(existsSync(configPath), `Config file should exist at ${configPath}`);
+    assert(existsSync(tokenizerPath), `Tokenizer file should exist at ${tokenizerPath}`);
+    for (const modalityPath of Object.values(modalityPaths)) {
+        assert(existsSync(modalityPath), `Modality file should exist at ${modalityPath}`);
+    }
+}
+
+async function testGetCheckpoint() {
+    console.log("- `testGetCheckpoint`: Start");
 
     try {
-        const modelId = 'unum-cloud/uform3-image-text-english-small';
-        const token = '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';
         const modalities = [Modality.TextEncoder, Modality.ImageEncoder];
 
-        const { configPath, modalityPaths, tokenizerPath } = await getCheckpoint(
-            modelId,
-            modalities,
-            token,
-            '.onnx'
-        );
+        for (const modelId of [
+            'unum-cloud/uform3-image-text-english-small',
+            'unum-cloud/uform3-image-text-english-base',
+            'unum-cloud/uform3-image-text-english-large',
+            'unum-cloud/uform3-image-text-multilingual-base',
+        ]) {
+            await tryGettingCheckpoint(modelId, modalities, hf_token);
+        }
 
-        assert(configPath !== null, "Config path should not be null");
-        assert(modalityPaths !== null, "Modality paths should not be null");
-        assert(tokenizerPath !== null, "Tokenizer path should not be null");
-
-        const textProcessor = new TextProcessor(configPath, tokenizerPath);
-        await textProcessor.init();
-        const processedTexts = await textProcessor.process("Hello, world!");
-
-        textEncoder = new TextEncoder(modalityPaths.text_encoder, textProcessor);
-        await textEncoder.init();
-        const textOutput = await textEncoder.forward(processedTexts);
-        console.log(textOutput.embeddings.dims);
-
-        const imageProcessor = new ImageProcessor(configPath);
-        await imageProcessor.init();
-        const processedImages = await imageProcessor.process("assets/unum.png");
-
-        imageEncoder = new ImageEncoder(modalityPaths.image_encoder, imageProcessor);
-        await imageEncoder.init();
-        const imageOutput = await imageEncoder.forward(processedImages);
-        console.log(imageOutput.embeddings.dims);
-
-        console.log("Test testEncoders: Success");
+        console.log("- `testGetCheckpoint`: Success");
     } catch (error) {
-        console.error("Test testEncoders: Failed", error);
-    } finally {
-        await textEncoder.dispose();
-        await imageEncoder.dispose();
+        console.error("- `testGetCheckpoint`: Failed", error);
+    }
+}
+
+async function tryTextEncoderForwardPass(modelId) {
+    const modalities = [Modality.TextEncoder];
+    const { configPath, modalityPaths, tokenizerPath } = await getCheckpoint(
+        modelId,
+        modalities,
+        hf_token,
+        '.onnx'
+    );
+
+    const textProcessor = new TextProcessor(configPath, tokenizerPath);
+    await textProcessor.init();
+    const processedTexts = await textProcessor.process("Hello, world!");
+
+    const textEncoder = new TextEncoder(modalityPaths.text_encoder, textProcessor);
+    await textEncoder.init();
+    const textOutput = await textEncoder.forward(processedTexts);
+    assert(textOutput.embeddings.dims.length === 2, "Output should be 2D");
+
+    await textEncoder.dispose();
+}
+
+async function tryImageEncoderForwardPass(modelId) {
+    const modalities = [Modality.ImageEncoder];
+    const { configPath, modalityPaths } = await getCheckpoint(
+        modelId,
+        modalities,
+        hf_token,
+        '.onnx'
+    );
+
+    const imageProcessor = new ImageProcessor(configPath);
+    await imageProcessor.init();
+    const processedImages = await imageProcessor.process("assets/unum.png");
+
+    const imageEncoder = new ImageEncoder(modalityPaths.image_encoder, imageProcessor);
+    await imageEncoder.init();
+    const imageOutput = await imageEncoder.forward(processedImages);
+    assert(imageOutput.embeddings.dims.length === 2, "Output should be 2D");
+
+    await imageEncoder.dispose();
+}
+
+async function testEncoders() {
+    console.log("- `testEncoders`: Start");
+
+    try {
+
+        // Go through the bi-modal models
+        for (const modelId of [
+            'unum-cloud/uform3-image-text-english-small',
+            'unum-cloud/uform3-image-text-english-base',
+            'unum-cloud/uform3-image-text-english-large',
+            'unum-cloud/uform3-image-text-multilingual-base',
+        ]) {
+            await tryTextEncoderForwardPass(modelId, hf_token);
+            await tryImageEncoderForwardPass(modelId, hf_token);
+        }
+
+        console.log("- `testEncoders`: Success");
+    } catch (error) {
+        console.error("- `testEncoders`: Failed", error);
     }
 }
 
