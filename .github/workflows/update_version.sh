95c91a9cb66de7ffa505e6ac328346fe886bfe18
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2024-10-01 13:31:29 -0500

    Make: Upgrade to TinySemVer

--- .github/workflows/update_version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo $1 > VERSION && 
-    sed -i "s/\"version\": \".*\"/\"version\": \"$1\"/" package.json &&
-    sed -i "s/^version: .*/version: $1/" CITATION.cff &&
-    sed -i "s/version = \".*\"/version = \"$1\"/" pyproject.toml
