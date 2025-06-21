ae290f1f712f05c010f856efdfb5647645dc9e6a
Gavrilikhin Daniil <gavrilikhin.daniil@gmail.com>
2025-06-21 19:14:43 +0300

    Improve: Support compiled CoreML models (#108)
    * Check for precompiled model URL
    * Don't threat compiled model path as dir
    * Try different approach

--- swift/Encoders.swift
+++ swift/Encoders.swift
@@ -99,7 +99,8 @@ func readConfig(fromPath path: String) throws -> [String: Any] {
 ///   - computeUnits: The hardware devices to use for model computation.
 /// - Returns: An instance of `MLModel`.
 func readModel(fromURL modelURL: URL, computeUnits: MLComputeUnits = .all) throws -> MLModel {
-    let compiledModelURL = try MLModel.compileModel(at: modelURL)
+    let compiledModelURL =
+        modelURL.absoluteString.hasSuffix(".mlmodelc/") ? modelURL : try MLModel.compileModel(at: modelURL)
     let config = MLModelConfiguration()
     config.computeUnits = computeUnits
     return try MLModel(contentsOf: compiledModelURL, configuration: config)
